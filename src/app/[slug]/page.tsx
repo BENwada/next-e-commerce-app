a59095231c99fa6fb89824c4cf2663a50d3617e0
BENwada <domo0103wada@gmail.com>
2024-07-30 18:32:00 +0900

    design fix

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import ProductImages from "@/components/ProductImages";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import Add from "@/components/Add";
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+
+      {/* TEXTS */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolor
+          iure error aliquid fuga alias asperiores soluta quod unde, tempore id
+          eligendi nostrum deserunt ex debitis atque, repudiandae dolorum!
+          Autem!
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            quae. A adipisci laboriosam temporibus blanditiis sapiente atque
+            necessitatibus quasi velit totam ipsa minima, hic voluptatibus
+            placeat consectetur quibusdam, itaque omnis.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            quae. A adipisci laboriosam temporibus blanditiis sapiente atque
+            necessitatibus quasi velit totam ipsa minima, hic voluptatibus
+            placeat consectetur quibusdam, itaque omnis.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            quae. A adipisci laboriosam temporibus blanditiis sapiente atque
+            necessitatibus quasi velit totam ipsa minima, hic voluptatibus
+            placeat consectetur quibusdam, itaque omnis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,8 +15,8 @@ const Footer = () => {
             3252 Winding Way, Central Plaza, Willowbrook, CA 90210, United
             States
           </p>
-          <span>hello@info.dev</span>
-          <span>+1 234 567 890</span>
+          <span className="font-semibold">hello@info.dev</span>
+          <span className="font-semibold">+1 234 567 890</span>
           <div className="flex gap-6">
             <Image src="/facebook.png" alt="" width={16} height={16} />
             <Image src="/instagram.png" alt="" width={16} height={16} />
@@ -26,7 +26,38 @@ const Footer = () => {
           </div>
         </div>
         {/* CENTER */}
-        <div className="hidden lg:flex justify-between w-1/2"></div>
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col gap-6 justify-between">
+            <h1 className="font-medium text-lg ">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About Us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affiliates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">New Arrivals</Link>
+              <Link href="">Accessories</Link>
+              <Link href="">Men</Link>
+              <Link href="">Women</Link>
+              <Link href="">All Products</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">Customer Service</Link>
+              <Link href="">My Account</Link>
+              <Link href="">Find a Store</Link>
+              <Link href="">Legal & Privacy</Link>
+              <Link href="">Gift Card</Link>
+            </div>
+          </div>
+        </div>
         {/* LIGHT */}
         <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <h1 className="font-medium text-lg">SUBSCRIBE</h1>
@@ -53,7 +84,9 @@ const Footer = () => {
         </div>
       </div>
       {/* BOTTOM */}
-      <div className=""></div>
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">© 2024 Lama Shop</div>
+      </div>
     </div>
   );
 };
