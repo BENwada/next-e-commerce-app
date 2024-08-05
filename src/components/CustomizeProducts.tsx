ce4ab5221e837d2e2399a2af58ce99953a3e9a81
BENwada <domo0103wada@gmail.com>
2024-08-06 08:18:09 +0900

    customizeProduct add

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -42,8 +42,8 @@ const CustomizeProducts = ({
     <div className="flex flex-col gap-6">
       {productOptions.map((option) => (
         <div className="flex flex-col gap-4" key={option.name}>
+          <h4 className="font-medium">Choose a {option.name}</h4>
           <ul className="flex items-center gap-3">
-            <h4 className="font-medium">Choose a {option.name}</h4>
             {option.choices?.map((choice) => {
               const disabled = !isVariantInStock({
                 ...selectedOptions,
@@ -53,17 +53,43 @@ const CustomizeProducts = ({
               const selected =
                 selectedOptions[option.name!] === choice.description;
 
-              return (
-                <div
-                  className="cursor-pointer"
-                  key={choice.value}
-                  onClick={() =>
-                    handleOptionSelect(option.name!, choice.description!)
-                  }
+              const clickHandler = disabled
+                ? undefined
+                : () => handleOptionSelect(option.name!, choice.description!);
+
+              return option.name === "color" ? (
+                <li
+                  className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative"
+                  style={{
+                    backgroundColor: choice.value,
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  onClick={clickHandler}
                 >
-                  {choice.description} {disabled && "disabled"}
-                  {selected && "selected"}
-                </div>
+                  {selected && (
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                  {disabled && (
+                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                </li>
+              ) : (
+                <li
+                  className="ring-1 ring-default text-default rounded-md py-1 px-4 text-sm cursor-pointer"
+                  style={{
+                    cursor: disabled ? "not-allowed" : "pointer",
+                    backgroundColor: selected
+                      ? "#f35c7a"
+                      : disabled
+                        ? "#FBCFE8"
+                        : "white",
+                    color: selected || disabled ? "white" : "#f35c7a",
+                    boxShadow: disabled ? "none" : "",
+                  }}
+                  onClick={clickHandler}
+                >
+                  {choice.description}
+                </li>
               );
             })}
           </ul>
